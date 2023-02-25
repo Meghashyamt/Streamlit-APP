d59db6fd3d3569797f704ba3fffc9eb99dcac8bb
Meghashyam <shyamms17@hotmail.com>
2023-02-25 15:37:49 +0530

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,20 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Set page title
-st.set_page_config(page_title="My First Streamlit App")
+st.set_page_config(page_title="Iris Data Visualization")
+
+# Load the Iris dataset
+iris_df = sns.load_dataset('iris')
 
 # Define the layout of the app
-st.title("My First Streamlit App")
-st.write("Welcome to my app!")
+st.title("Iris Data Visualization")
+st.write("This app displays some visualizations of the Iris dataset.")
 
-# Generate some random data
-df = pd.DataFrame(np.random.randn(50, 2), columns=['x', 'y'])
+# Display the dataset
+st.subheader("Iris Dataset")
+st.write(iris_df)
 
-# Create a scatter plot using Plotly Express
-fig = px.scatter(df, x='x', y='y')
+# Create a scatter plot
+st.subheader("Scatter Plot")
+sns.set_style("darkgrid")
+sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=iris_df)
+st.pyplot()
 
-# Display the plot
-st.plotly_chart(fig)
+# Create a histogram
+st.subheader("Histogram")
+sns.histplot(x="petal_length", hue="species", data=iris_df, kde=True)
+st.pyplot()
+
+# Create a boxplot
+st.subheader("Box Plot")
+sns.boxplot(x="species", y="petal_width", data=iris_df)
+st.pyplot()
+
+# Create a pairplot
+st.subheader("Pair Plot")
+sns.pairplot(data=iris_df, hue="species")
+st.pyplot()
